d0e806ebdda88f7bdea19f9acefc11f480a1f62b
phuongnm153 <nghiemminhphuong@gmail.com>
2013-09-30 20:51:28 +0700

    bo sung 2 file viewcontroller 2

---
+++ Demogit/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  Demogit
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ Demogit/ViewController2.m
@@ -0,0 +1,38 @@
+//
+//  ViewController2.m
+//  Demogit
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
